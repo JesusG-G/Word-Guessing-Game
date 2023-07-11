fb543e6d63fa1bb578c0f877c4f3f76e37298651
JesusG-G <jesusguillermogonzalezguaita@gmail.com>
2023-07-11 19:58:46 -0400

    [JESUS] -  Update main file with the typing of the variables and function determinate_winner

--- src/main.py
+++ src/main.py
@@ -2,36 +2,30 @@ import random
 import os
 
 #Function to create the file path with the wordlist.txt
-def file_path_complete():
+def file_path_complete() -> str:
     current_working_directory = os.getcwd()
     file_path = os.path.join(current_working_directory,'src\wordlist.txt')
     return file_path
 
 #Function to choose the word of the list
-def choose_the_word(file_path):
+def choose_the_word(file_path: str) -> str:
     with open(file_path) as file:
-        wordlist = file.read().splitlines()
-    chosen_word = random.choice(wordlist)
+        wordlist: list[str] = file.read().splitlines()
+    chosen_word: str = random.choice(wordlist)
     return chosen_word
 
-def determinate_winner(chosen_word):
-    minimun_attemp = len(chosen_word) + 2
-    guess_attempt = 0
-    guesses = []
-    chosen_word_list = []
-    letter_guessed = ''
-    guess_loop = False
-    winner = False
+def determinate_winner(chosen_word: str) -> bool:
+    minimun_attemp: int = len(chosen_word) + 2
+    letter_guessed: str = ''
+    guess_loop: bool = False
     #append the underscore on the guesses list
-    for i in range(len(chosen_word)):
-        guesses.append("_")
+    guesses:list[str] = ["_" for _ in range(len(chosen_word))]
     #create a list of the chosen word
-    for c in chosen_word:
-            chosen_word_list.append(c)
+    chosen_word_list: list[str] = [c for c in chosen_word]
 
-    while guess_attempt != minimun_attemp and winner != True:
+    while minimun_attemp >= 0:
         while guess_loop != True:
-            guess_character = str(input("Please enter your guess character: "))
+            guess_character:str = str(input("Please enter your guess character: "))
             if not guess_character.isalpha():
                 print("Please enter only a Letter")
             elif guess_character in letter_guessed:
@@ -40,38 +34,40 @@ def determinate_winner(chosen_word):
                 guess_loop = True
             else:
                 print("Please enter ONLY one character.")
-        for i in range(len(chosen_word)):
-            if guess_character in chosen_word:
-                if guess_character == chosen_word[i]:
+
+        for word_character in chosen_word:
+            if guess_character == word_character:
+                index: int = chosen_word_list.index(guess_character)
                     #Save the guess character on the guess list
-                    guesses[i] = guess_character
+                guesses[index] = guess_character
                     #Replace in the chosen word list the guess character for underscore
-                    chosen_word_list[i] = '_'
+                chosen_word_list[index] = '_'
                     #update the chosen word
-                    chosen_word = ''.join(chosen_word_list)
-                    print(f"The {guess_character} is in the chosen word")
-                    print(f"You have {minimun_attemp} tries")
-                    guess_loop = False
-                    letter_guessed += guess_character
-                    #If guesses (like a string) is equal to the copy of chosen word the first while loop will finish
-                    if ''.join(guesses) == chosen_word_copy:
-                        winner = True
-                else:
-                    guess_loop = False
+                chosen_word = ''.join(chosen_word_list)
+                print(chosen_word)
+                print(f"The {guess_character} is in the chosen word")
+                print(f"You have {minimun_attemp} tries yet")
+                guess_loop = False
+                letter_guessed += guess_character
             else:
                 letter_guessed += guess_character
-                minimun_attemp -=1
                 guess_loop = False
-                print(f"You have {minimun_attemp} tries")
-                break
+        if guess_character not in chosen_word_copy:
+            minimun_attemp -=1
+            print(f"Wrong!! You have {minimun_attemp} attempts to try yet")
+            letter_guessed += guess_character
+            guess_loop = False
+            if minimun_attemp == 0:
+                print("No more attempts")
+                return False
+        elif ''.join(guesses) == chosen_word_copy:
+            return True
         print(f"Your progress is {''.join(guesses)}")
-    
-    return winner
 
 if __name__ == "__main__":
     #call of the function 
-    file_path = file_path_complete()
-    chosen_word = choose_the_word(file_path)
+    file_path: str = file_path_complete()
+    chosen_word:str  = choose_the_word(file_path)
     #A little presentation
     print("Welcome to the Word Guessing Game")
     print("------------------------------------------------------------------")
@@ -84,8 +80,8 @@ if __name__ == "__main__":
     -----------------------------------------------------------------------------------
     """)
     #Create a copy of the chosen word
-    chosen_word_copy = chosen_word
-    winner = determinate_winner(chosen_word)
+    chosen_word_copy:str = chosen_word
+    winner: bool = determinate_winner(chosen_word)
     if winner:
         print(f"Congratulations you won! The word was {chosen_word_copy}")
     else:
